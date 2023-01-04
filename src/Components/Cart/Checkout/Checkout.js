cc05a149803f1e86b5d2677c0428203f4bf74f42
Emmanuel <emmychris477@gmail.com>
2023-01-04 20:49:47 +0100

    hooked input entries with useRef

--- src/Components/Cart/Checkout/Checkout.js
+++ src/Components/Cart/Checkout/Checkout.js
@@ -1,36 +1,49 @@
+import { useRef } from "react";
 import "./Checkout.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const adressInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredAddress = adressInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
     <form onSubmit={confirmHandler}>
       <div className="control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name"></input>
+        <input type="text" id="name" ref={nameInputRef}></input>
       </div>
 
       <div className="control">
         <label htmlFor="street">Address</label>
-        <input type="text" id="street"></input>
+        <input type="text" id="street" ref={adressInputRef}></input>
       </div>
 
       <div className="control">
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal"></input>
+        <input type="text" id="postal" ref={postalCodeInputRef}></input>
       </div>
 
       <div className="control">
         <label htmlFor="city">City/State</label>
-        <input type="text" id="city"></input>
+        <input type="text" id="city" ref={cityInputRef}></input>
       </div>
 
-      <button type="button" onClick={props.cancelOrder}>
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className="actions">
+        <button type="button" onClick={props.cancelOrder}>
+          Cancel
+        </button>
+        <button>Confirm</button>
+      </div>
     </form>
   );
 };
