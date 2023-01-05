0be50ee057183ded7e284833c34f7846f94a219a
Emmanuel <emmychris477@gmail.com>
2023-01-05 23:20:06 +0100

    basic validation with hooked up useRef values

--- src/Components/Cart/Checkout/Checkout.js
+++ src/Components/Cart/Checkout/Checkout.js
@@ -1,7 +1,17 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./Checkout.css";
 
+const isNotEmpty = (value) => value.trim() !== "";
+const isSixChars = (value) => value.length === 6;
+
 const Checkout = (props) => {
+  const [formInputValidity, setFormInputvalidity] = useState({
+    name: true,
+    address: true,
+    postalcode: true,
+    city: true,
+  });
+
   const nameInputRef = useRef();
   const adressInputRef = useRef();
   const postalCodeInputRef = useRef();
@@ -14,35 +24,81 @@ const Checkout = (props) => {
     const enteredAddress = adressInputRef.current.value;
     const enteredPostalCode = postalCodeInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
+
+    const enteredNameIsValid = isNotEmpty(enteredName);
+    const enteredAddressIsValid = isNotEmpty(enteredAddress);
+    const enteredPostalCodeIsvalid = isSixChars(enteredPostalCode);
+    const enteredCityIsValid = isNotEmpty(enteredCity);
+
+    setFormInputvalidity({
+      name: enteredNameIsValid,
+      address: enteredAddressIsValid,
+      postalcode: enteredPostalCodeIsvalid,
+      city: enteredCityIsValid,
+    });
+
+    const formIsvalid =
+      enteredNameIsValid &&
+      enteredAddressIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsvalid;
+
+    if (!formIsvalid) {
+      return;
+    }
+
+    //submit Cart data to db
   };
 
   return (
     <form onSubmit={confirmHandler}>
-      <div className="control">
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" ref={nameInputRef}></input>
-      </div>
+      <div>
+        <div className="control">
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" ref={nameInputRef}></input>
+          {!formInputValidity.name && (
+            <p style={{ color: "red", margin: "0" }}>
+              Please enter a valid name
+            </p>
+          )}
+        </div>
 
-      <div className="control">
-        <label htmlFor="street">Address</label>
-        <input type="text" id="street" ref={adressInputRef}></input>
-      </div>
+        <div className="control">
+          <label htmlFor="street">Address</label>
+          <input type="text" id="street" ref={adressInputRef}></input>
+          {!formInputValidity.address && (
+            <p style={{ color: "red", margin: "0" }}>
+              Please enter a valid address
+            </p>
+          )}
+        </div>
 
-      <div className="control">
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" ref={postalCodeInputRef}></input>
-      </div>
+        <div className="control">
+          <label htmlFor="postal">Postal Code</label>
+          <input type="text" id="postal" ref={postalCodeInputRef}></input>
+          {!formInputValidity.postalcode && (
+            <p style={{ color: "red", margin: "0" }}>
+              Please enter a valid postal code (1 - 6 characters)
+            </p>
+          )}
+        </div>
 
-      <div className="control">
-        <label htmlFor="city">City/State</label>
-        <input type="text" id="city" ref={cityInputRef}></input>
-      </div>
+        <div className="control">
+          <label htmlFor="city">City/State</label>
+          <input type="text" id="city" ref={cityInputRef}></input>
+          {!formInputValidity.city && (
+            <p style={{ color: "red", margin: "0" }}>
+              Please enter a valid city
+            </p>
+          )}
+        </div>
 
-      <div className="actions">
-        <button type="button" onClick={props.cancelOrder}>
-          Cancel
-        </button>
-        <button>Confirm</button>
+        <div className="actions">
+          <button type="button" onClick={props.cancelOrder}>
+            Cancel
+          </button>
+          <button>Confirm</button>
+        </div>
       </div>
     </form>
   );
